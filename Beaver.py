0a60f91330e253bb6a9dae0d85479bd8b89ee0a8
f0906739 <f0906739@students.katyisd.org>
2020-02-07 18:18:35 -0600

    Redid Beaver Time class

--- Beaver.py
+++ Beaver.py
@@ -1,4 +1,18 @@
 class Beaver():
-    
     def sleep():
         pass
+
+
+class Time():
+    def __init__(self, days=0, hours=0, minutes=0):
+        self.days = days
+        self.hours = hours
+        self.minutes = minutes
+
+    def calculate_time(self):
+        while self.minutes > 59:
+            self.minutes -= 60
+            self.hours += 1
+        while self.hours > 23:
+            self.hours -= 24
+            self.days += 1
--- test_Beaver.py
+++ test_Beaver.py
@@ -1,17 +1,29 @@
 import unittest
+from Beaver import Time, Beaver
 
 
 class TestTime(unittest.TestCase):
     def setUp(self):
-        pass
+        self.time = Time(1, 73, 63)
 
     def tearDown(self):
-        pass
+        print("tearDown\n")
 
     @classmethod
     def setUpClass(cls):
-        print('Setup class')
+        print('Setup class\n')
 
     @classmethod
     def tearDownClass(cls):
-        print('Teardown class')
+        print('Teardown class\n')
+
+    def test_calculate_time(self):
+        self.time.calculate_time()
+
+        self.assertEqual(self.time.minutes, 3)
+        self.assertEqual(self.time.hours, 2)
+        self.assertEqual(self.time.days, 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
